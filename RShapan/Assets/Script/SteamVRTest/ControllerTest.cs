a46b1478bd6d7d8a688f125b87a0c8b4148c5932
hastar <745377247@qq.com>
2021-11-11 22:57:00 +0800

    SteamVR的基本操作

--- RShapan/Assets/Script/SteamVRTest/ControllerTest.cs
+++ RShapan/Assets/Script/SteamVRTest/ControllerTest.cs
@@ -6,15 +6,66 @@ public class ControllerTest : MonoBehaviour {
 	SteamVR_TrackedObject trackedObject;
 	SteamVR_Controller.Device device;
     GameObject interactBox = null;
+    Transform moveCube;
 	// Use this for initialization
 	void Start () {
         //获取被追踪的对象
         trackedObject = GetComponent<SteamVR_TrackedObject>();
         //获取对应设备
         device = SteamVR_Controller.Input((int)trackedObject.index);
+        moveCube = GameObject.Find("MoveCube").transform;
 		
 	}
 	void Update () {
+
+	}
+
+    public void MoveCube()
+    {
+        //判断是否按着触摸盘
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            //获取触摸盘的坐标(x, y) [-1, 1]
+            Vector2 dir = device.GetAxis();
+            //边界处理
+            if (moveCube != null)
+            {
+                //移动方向
+                Vector3 vDir = new Vector3(dir.x, 0, dir.y);
+                //移动物体
+                moveCube.position += vDir * Time.deltaTime * 3;
+            }
+        }
+    }
+
+    public void PickObject()
+    {
+        //判断按键状态  按下按键
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //判断是否碰撞
+            if (interactBox != null)
+            {
+                //通过设置父物体， 实现物体跟随
+                interactBox.transform.parent = transform;
+                //去掉重力， 让物体不掉落
+                interactBox.GetComponent<Rigidbody>().useGravity = false;
+            }
+        }
+
+        //松开按键， 状态移除
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            if (interactBox != null)
+            {
+                interactBox.transform.parent = transform;
+                interactBox.GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+    }
+
+    public void ChangeColor()
+    {
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
         {
             SetInteractBoxColor(Color.red);
@@ -23,7 +74,7 @@ public class ControllerTest : MonoBehaviour {
         {
             SetInteractBoxColor(Color.yellow);
         }
-	}
+    }
 
     void SetInteractBoxColor(Color r){
         if(interactBox != null)
@@ -36,11 +87,6 @@ public class ControllerTest : MonoBehaviour {
         Debug.Log(other.gameObject.name);
         interactBox = other.gameObject;
         SetInteractBoxColor(Color.yellow);
-        device.TriggerHapticPulse(5000, Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad);
-        device.TriggerHapticPulse(5000, Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
-        device.TriggerHapticPulse(5000, Valve.VR.EVRButtonId.k_EButton_Grip);
-        device.TriggerHapticPulse(5000, Valve.VR.EVRButtonId.k_EButton_ApplicationMenu);
-        device.TriggerHapticPulse(5000, Valve.VR.EVRButtonId.k_EButton_System);
 
     }
 	void OnTriggerExit(Collider other){
